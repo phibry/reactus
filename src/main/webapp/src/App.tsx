6e1700d3042df0556a5d57aa9e5c90854f37caa5
Philipp Rieser <philipp.rieser@gmail.com>
2022-10-10 09:09:39 +0200

    add cra & mui

---
+++ src/main/webapp/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material';
+
+const App = () => {
+  return (
+    <>
+      <Button variant='outlined'>My button</Button>
+      <div>test</div>
+    </>
+  );
+};
+
+export default App;
